c457eb3534437c415f401eb2f0a95c0f161362b9
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-12-06 11:23:23 +0530

    new updates

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,7 +8,7 @@ terraform {
   backend "s3" {
     bucket = "fusion-iac-backend-royal"
     key    = "my-practice"
-    region = "ap-south-1"
+    region = "us-east-1"
     dynamodb_table = "tflock"
   }
 }
