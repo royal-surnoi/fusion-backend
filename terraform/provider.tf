854c68a7dc298a3877a629dfb2543aa504d426e0
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-12-06 11:19:29 +0530

    backend terraform lock

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "5.78.0"
     }
   }
+  backend "s3" {
+    bucket = "fusion-iac-backend-royal"
+    key    = "my-practice"
+    region = "ap-south-1"
+    dynamodb_table = "tflock"
+  }
 }
 
 provider "aws" {
